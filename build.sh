ccf76b1153ec7f34a452250940daefd47e7a0e6a
Patrice Lachance <patlachance@gmail.com>
2021-04-06 17:09:10 +0200

    CIP Initial release

---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Description: container image build & push script
+# Author: Patrice Lachance
+##############################################################################
+
+opt_push=$1
+IMG_NAME="quay.io/cip-core-platform/pod-identity-webhook"
+BUILDER_IMAGE=docker.io/golang:latest
+RUNTIME_IMAGE=registry.access.redhat.com/ubi8/ubi-minimal:latest
+
+IMG_TAG="$(getcurrent_docker.sh)"
+RELEASE="$IMG_TAG"
+VERSION="$IMG_TAG"
+BUILD_DATE="$(date +'%Y-%M-%d'T'%H:%M:%S%Z')"
+
+# Pulling latest images
+docker pull $BUILDER_IMAGE
+docker pull $RUNTIME_IMAGE
+
+# Extracting builer images characteristics
+BUILDER_GOLANG_VERSION=$(docker inspect $BUILDER_IMAGE | jq -r '.[0].Config.Env | tostring | capture("(?<var>[a-z_A-Z]+)=(?<val>[0-9.]+)") | .val')
+RUNTIME_BUILD_DATE=$(docker inspect $RUNTIME_IMAGE | jq -r '.[0].Config.Labels."build-date"')
+RUNTIME_OS_RELEASE=$(docker run -it --rm $RUNTIME_IMAGE grep PRETTY_NAME /etc/os-release | cut -d= -f2 | sed -e 's/"//g' -e 's/\r//')
+
+
+# Building our image
+docker build -t $IMG_NAME:$IMG_TAG \
+             --build-arg BUILDER_IMAGE=$BUILDER_IMAGE \
+             --build-arg RUNTIME_IMAGE=$RUNTIME_IMAGE \
+             --label io.cip.release="$RELEASE" \
+             --label io.cip.version="$VERSION" \
+             --label io.cip.runtime-image-name="$RUNTIME_IMAGE" \
+             --label io.cip.runtime-os-release="$RUNTIME_OS_RELEASE" \
+             --label io.cip.builder-image-name="$BUILDER_IMAGE" \
+             --label io.cip.builder-golang-version="$BUILDER_GOLANG_VERSION" \
+             --label io.cip.build-date="$BUILD_DATE" \
+             --label release="$RELEASE" \
+             --label version="$VERSION" \
+             --label build-date="$BUILD_DATE" \
+             -f Dockerfile.ubi8 .
+
+if [ "x$opt_push" == "xpush" ]; then
+  docker push $IMG_NAME:$IMG_TAG
+fi
+
